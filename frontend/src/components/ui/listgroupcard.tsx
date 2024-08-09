9a0a28d63aba1f3cb7baeda9b1a8c2ad3850c624
Phatdanai Shinpanjapol <petersafe45@gmail.com>
2024-08-09 13:48:21 +0700

    [Add] workspace somponents

---
+++ frontend/src/components/ui/listgroupcard.tsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+import { cn } from "@/lib/utils"
+import { GroupCardProps } from "./groupcard"
+
+type ListGroupCardProps = {
+    card: GroupCardProps[]
+    x?: number[]
+}
+
+
+
+const ListGroupCard: React.FC<ListGroupCardProps> = ({
+    card,
+
+}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ListGroupCard;
