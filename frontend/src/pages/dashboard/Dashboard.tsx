8e615ec0c7ad689bd1f4306142ddfed893ab35ec
Phatdanai Shinpanjapol <petersafe45@gmail.com>
2024-08-23 01:27:44 +0700

    [ADD] testing fetch back to front

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,14 +1,51 @@
 import SideBarItem from "@/components/layout/SideBarItem.tsx"
 import SideBar from "@/components/layout/SideBar.tsx"
 import MainPanel from "@/components/layout/MainPanel.tsx"
+import { Button } from "@/components/ui/button"
+import WorkspaceCard from "./components/Workspace.tsx"
+import { useGetListWorkspace } from "@/hooks/useGetListWorkspace.ts"
 
 const Dashboard = () => {
+  const { data } = useGetListWorkspace()
+
   return (
     <>
       <SideBar isSignOutEnabled={true}>
-        <SideBarItem title={"Home"} isActive={true} />
+        <SideBarItem title={"Candidate"} isActive={true} />
+        <SideBarItem title={"Assessment"} isActive={false} onClick={() => {}} />
       </SideBar>
-      <MainPanel></MainPanel>
+      <MainPanel>
+        <Button
+          className={
+            "w-52 h-14 text-center font-semibold text-xl rounded-xl disabled:opacity-100"
+          }
+          onClick={() => {}}
+        >
+          Add new group
+        </Button>
+        {data?.data == null ? (
+          <></>
+        ) : (
+          data.data.map((Workspace) => {
+            if (
+              Workspace.title == undefined ||
+              Workspace.startdate == undefined
+            ) {
+              return <></>
+            } else {
+              return (
+                <>
+                  <WorkspaceCard
+                    title={Workspace.title}
+                    createAt={Workspace.startdate}
+                    member={10}
+                  />
+                </>
+              )
+            }
+          })
+        )}
+      </MainPanel>
     </>
   )
 }
---
+++ frontend/src/pages/dashboard/components/Workspace.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
+export type WorkspaceProps = {
+  title: string
+  member: number
+  // user: User[]
+  createAt: string
+}
+
+const WorkspaceCard: React.FC<WorkspaceProps> = ({
+  title,
+  member,
+  createAt,
+}) => {
+  return (
+    <div
+      className={cn(
+        "w-full p-4 text-center font-semibold text-xl rounded-xl bg",
+      )}
+    >
+      <div className="text-primary">{title}</div>
+      <div>{member > 1 ? member + "Candidates" : member + "Candidate"}</div>
+      <div>Created At: {createAt}</div>
+    </div>
+  )
+}
+
+export default WorkspaceCard
