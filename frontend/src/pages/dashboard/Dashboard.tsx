92a8b71f99113d3b23795ace7e8591d3270110c2
Phatdanai Shinpanjapol <petersafe45@gmail.com>
2024-09-02 22:27:57 +0700

    [ADD] Page & CSV Parse
    Create workspace Page struct
    file email import

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -2,11 +2,39 @@ import SideBarItem from "@/components/layout/SideBarItem.tsx"
 import SideBar from "@/components/layout/SideBar.tsx"
 import MainPanel from "@/components/layout/MainPanel.tsx"
 import { Button } from "@/components/ui/button"
-import WorkspaceCard from "./components/Workspace.tsx"
 import { useGetListWorkspace } from "@/hooks/useGetListWorkspace.ts"
+import { useNavigate } from "react-router-dom"
+import ListWorkspaceCard from "./components/ListWorkspace.tsx"
+import { Input } from "@/components/ui/input.tsx"
+import Papa from "papaparse"
+import React, { useState } from "react"
+import { server } from "@/contexts/swr.tsx"
 
 const Dashboard = () => {
   const { data } = useGetListWorkspace()
+  const navigate = useNavigate()
+  const [userData, setUserData] = useState<DataRow[]>()
+
+  interface DataRow {
+    [key: string]: string
+  }
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+    if (file) {
+      Papa.parse(file, {
+        skipEmptyLines: true,
+        complete: (results) => {
+          setUserData(results.data as DataRow[])
+          console.log(results)
+        },
+      })
+    }
+  }
+
+  const handleSubmitFile = () => {
+    server.user
+  }
 
   return (
     <>
@@ -15,36 +43,43 @@ const Dashboard = () => {
         <SideBarItem title={"Assessment"} isActive={false} onClick={() => {}} />
       </SideBar>
       <MainPanel>
-        <Button
-          className={
-            "w-52 h-14 text-center font-semibold text-xl rounded-xl disabled:opacity-100"
-          }
-          onClick={() => {}}
-        >
-          Add new group
-        </Button>
-        {data?.data == null ? (
-          <></>
-        ) : (
-          data.data.map((Workspace) => {
-            if (
-              Workspace.title == undefined ||
-              Workspace.startdate == undefined
-            ) {
-              return <></>
-            } else {
-              return (
-                <>
-                  <WorkspaceCard
-                    title={Workspace.title}
-                    createAt={Workspace.startdate}
-                    member={10}
-                  />
-                </>
-              )
+        <div className="w-full mt-8 p-10 grid gap-10">
+          <div className="flex justify-between">
+            <p className="text-5xl font-bold text-gray-900">Candidate Group</p>
+            <Button
+              className={
+                "w-52 h-14 text-center font-semibold text-xl rounded-xl disabled:opacity-100"
+              }
+              onClick={() => {
+                navigate("create")
+              }}
+            >
+              Add new group
+            </Button>
+          </div>
+
+          <Input
+            className="w-64"
+            type="file"
+            accept=".csv"
+            id="userMail"
+            onChange={(e) => {
+              handleFileUpload(e)
+              console.log(userData)
+            }}
+          />
+
+          <Button
+            className={
+              "w-52 h-14 text-center font-semibold text-xl rounded-xl disabled:opacity-100"
             }
-          })
-        )}
+            onClick={() => {}}
+          >
+            Submit
+          </Button>
+
+          <ListWorkspaceCard workspace={data?.data ?? []} />
+        </div>
       </MainPanel>
     </>
   )
