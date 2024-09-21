d708a0407a44f52fd3afc59d3e2119f8a8f6adf7
Phatdanai Shinpanjapol <petersafe45@gmail.com>
2024-09-22 01:29:24 +0700

    [update] refactor code to component

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -5,36 +5,10 @@ import { Button } from "@/components/ui/button"
 import { useGetListWorkspace } from "@/hooks/useGetListWorkspace.ts"
 import { useNavigate } from "react-router-dom"
 import ListWorkspaceCard from "./components/ListWorkspace.tsx"
-import { Input } from "@/components/ui/input.tsx"
-import Papa from "papaparse"
-import React, { useState } from "react"
-// import { server } from "@/contexts/swr.tsx"
 
 const Dashboard = () => {
   const { data } = useGetListWorkspace()
   const navigate = useNavigate()
-  const [userData, setUserData] = useState<DataRow[]>()
-
-  interface DataRow {
-    [key: string]: string
-  }
-
-  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0]
-    if (file) {
-      Papa.parse(file, {
-        skipEmptyLines: true,
-        complete: (results) => {
-          setUserData(results.data as DataRow[])
-          console.log(results)
-        },
-      })
-    }
-  }
-
-  // const handleSubmitFile = () => {
-  //   server.user
-  // }
 
   return (
     <>
@@ -57,26 +31,6 @@ const Dashboard = () => {
               Add new group
             </Button>
           </div>
-
-          <Input
-            className="w-64"
-            type="file"
-            accept=".csv"
-            id="userMail"
-            onChange={(e) => {
-              handleFileUpload(e)
-              console.log(userData)
-            }}
-          />
-
-          <Button
-            className={
-              "w-52 h-14 text-center font-semibold text-xl rounded-xl disabled:opacity-100"
-            }
-            onClick={() => {}}
-          >
-            Submit
-          </Button>
 
           <ListWorkspaceCard workspace={data?.data ?? []} />
         </div>
